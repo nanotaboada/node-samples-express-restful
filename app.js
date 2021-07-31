17b1b20ddf054cc4e52390d6fae93a198cbd2061
Nano Taboada <nanotaboada@users.noreply.github.com>
2021-07-31 16:03:10 -0300

    Added logging library

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const winston = require('winston');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  // defaultMeta: { service: 'user-service' },
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple(),
+  }));
+}
+
 app.listen(port, () => {
-  console.log(`[RUNNING] on port ${port}`);
+  logger.info(`running on port ${port}`);
 });
