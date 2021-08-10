e10821bf94f01c178a758bae50ebfbf9c02e6b76
Nano Taboada <nanotaboada@users.noreply.github.com>
2021-08-09 22:31:57 -0300

    Initial suite of integration tests

--- app.js
+++ app.js
@@ -1,28 +1,7 @@
 /* eslint-disable import/extensions */
 
 import express from 'express';
-import winston from 'winston';
 import routes from './routes/songsRoutes.js';
-
-/* -----------------------------------------------------------------------------
-  Logging
------------------------------------------------------------------------------ */
-
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  defaultMeta: { module: 'app.js' },
-  transports: [
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
-  ],
-});
-
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple(),
-  }));
-}
 
 /* -----------------------------------------------------------------------------
   Application
@@ -33,7 +12,4 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', routes);
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  logger.info(`running on port ${port}`);
-});
+export default app;
---
+++ tests/songsTests.js
@@ -0,0 +1,21 @@
+/* eslint-disable import/extensions */
+
+import supertest from 'supertest';
+import { should } from 'chai';
+import app from '../app.js';
+
+const request = supertest(app);
+should();
+
+describe('HTTP GET', () => {
+  describe('/api/v1/songs', () => {
+    it('When request has no parameters, then response Status-Code should be 200 (OK)', (done) => {
+      request
+        .get('/api/v1/songs')
+        .end((error, response) => {
+          response.statusCode.should.equal(200);
+          done();
+        });
+    });
+  });
+});
