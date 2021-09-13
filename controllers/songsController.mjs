3af74f3c5356e8e5250a3a1c010f25bda2b004b2
Nano Taboada <nanotaboada@users.noreply.github.com>
2021-09-13 16:27:15 -0300

    Initial suite of unit tests

--- controllers/songsController.mjs
+++ controllers/songsController.mjs
@@ -13,21 +13,29 @@ const songsController = {
 
   getSongsByYear(request, response) {
     const year = parseInt(request.params.id, 10);
-    const songsByYear = service.retrieveSongsByYear(year);
-    if (songsByYear) {
-      response.status(200).json(songsByYear).end();
+    const { min, max } = service.getRangeOfYears();
+    if (year >= min && year <= max) {
+      const songsByYear = service.filterSongsByYear(year.toString());
+      if (songsByYear) {
+        response.status(200).json(songsByYear).end();
+      } else {
+        response.status(404).end();
+      }
     } else {
-      response.status(404).end();
+      response.status(422).end();
     }
   },
 
   getSongByRank(request, response) {
     const rank = parseInt(request.params.id, 10);
-    const songByRank = service.retrieveSongsByRank(rank);
-    if (songByRank) {
-      response.status(200).json(songByRank).end();
+    const { min, max } = service.getRangeOfRanks();
+    if (rank >= min && rank <= max) {
+      const songByRank = service.findSongByRank(rank);
+      if (songByRank) {
+        response.status(200).json(songByRank).end();
+      }
     } else {
-      response.status(404).end();
+      response.status(422).end();
     }
   },
 };
